c945d120fa743bcb6345ddc40401530648c2a0ed
Jason McGloin <jason.mcgloin@gmail.com>
2019-12-05 23:29:46 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 class Formatter {
-  //add static methods here
+	static capitalize(string) {
+		return string[0].toUpperCase() + string.substring(1)
+	}
+
+	static sanitize(string) {
+		return string.replace(/[^A-Za-z0-9\-\'\s]/gi, '')
+	}
+
+	static titleize(string) {
+		const exclude = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+		return string.split(' ').map((word, i) => !exclude.includes(word) || i == 0 ? Formatter.capitalize(word) : word).join(' ')
+	}
 }
